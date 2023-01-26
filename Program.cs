e084177f7b8d37c8eee8478ef88ef4d98966dc69
LichiCat <antonovn1337@mail.ru>
2023-01-26 21:08:19 +0300

    New request with amendments

--- Program.cs
+++ Program.cs
@@ -4,64 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        string grif; //переменная для материала грифа 
-        string body; //переменная для материал деки гитары
-        int frets;   //переменная для колисчества ладов
-        int weight;  //переменная для веса
-        int price;   //переманная для стоимости 
+        //Материал грифа.
+        string grif; 
 
+        //Материал деки гитары.
+        string body; 
+
+        //Колисчество ладов.
+        int frets;   
+
+        //Вес.
+        int weight;
+        
+        //Стоимость.
+        int price;   
+
+        //Переменные для цикла вывода значений.
         int k=1, t;
 
         Console.WriteLine("Введите материал грифа"); 
-        grif = Console.ReadLine();                         //ввод значения для материала грифа 1
+        grif = Console.ReadLine();
 
         Console.WriteLine("Введите материал деки гитары"); 
-        body = Console.ReadLine();                         //ввод значения для материала деки 2
+        body = Console.ReadLine();                         
 
         Console.WriteLine("Введите количество ладов гитары");
-        frets = Convert.ToInt32(Console.ReadLine());       //ввод значения для количества ладов 3
+        frets = Convert.ToInt32(Console.ReadLine());       
 
         Console.WriteLine("Введите вес гитары в килограммах");
-        weight = Convert.ToInt32(Console.ReadLine());      //ввод значения для веса 4
+        weight = Convert.ToInt32(Console.ReadLine());      
 
         Console.WriteLine("Введите стоимость гитары");
-        price = Convert.ToInt32(Console.ReadLine());       //ввод значения для стоимости 5
+        price = Convert.ToInt32(Console.ReadLine());       
 
         Console.WriteLine("Чтобы вывести конкретные данные о гитаре, воспользуетесь встроенной клавиатурой, \n" +
                 "где 1-Материал грифа,  2-Материал деки, 3-Количество ладов, 4-Вес, 5-Стоимость\n" +
                 "Для выхода введите значение  0");
+
         while (k != 0)
         { 
             t = Convert.ToInt32(Console.ReadLine());
 
             if (t == 0)
-            {
-                k = 0; // выход из цикла
+            {   
+                // выход из цикла
+                k = 0; 
             }
 
             if (t == 1)
             {
-                Console.WriteLine("Материал грифа: " + grif); // 1
+
+                Console.WriteLine("Материал грифа: " + grif); 
             }
 
             if (t == 2)
             {
-                Console.WriteLine("Материал деки гитары: " + body); // 2
+                Console.WriteLine("Материал деки гитары: " + body); 
             }
 
             if (t == 3)
             {
-                Console.WriteLine("Количество ладов гитары: " + frets); // 3
+                Console.WriteLine("Количество ладов гитары: " + frets); 
             }
 
             if (t == 4)
             {
-                Console.WriteLine("Вес гитары: " + weight); // 4
+                Console.WriteLine("Вес гитары: " + weight); 
             }
 
             if (t == 5)
             {
-                Console.WriteLine("Стоимость гитары: " + price); //5
+                Console.WriteLine("Стоимость гитары: " + price); 
             }
         }
     }
